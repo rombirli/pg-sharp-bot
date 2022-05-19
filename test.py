5b4cece11e72f1fae9d0cea4faad2c1989fe4860
Romain Birling <romain.birling@epfl.ch>
2022-05-19 20:52:50 +0200

    added figures

--- test.py
+++ test.py
@@ -5,22 +5,31 @@ from processing.filtering import Filters
 from android.controller import screenshot
 
 image = screenshot()
+plt.imshow(image),
+plt.title('original')
+plt.savefig('figures/original.png')
+plt.show()
 plt.imshow(Filters.pgsharp(image)),
 plt.title('pgsharp')
+plt.savefig('figures/pgsharp.png')
 plt.show()
 plt.imshow(Filters.pokestops(image))
 plt.title('pokestops')
+plt.savefig('figures/pokestops.png')
 plt.show()
 plt.imshow(Filters.sea(image))
 plt.title('sea')
+plt.savefig('figures/sea.png')
 plt.show()
 plt.imshow(Filters.road_grey(image))
 plt.title('road grey')
+plt.savefig('figures/road_grey.png')
 plt.show()
 plt.imshow(Filters.road_yellow(image))
 plt.title('road yellow')
+plt.savefig('figures/road_yellow.png')
 plt.show()
 plt.imshow(Filters.land(image))
 plt.title('land')
+plt.savefig('figures/land.png')
 plt.show()
-
