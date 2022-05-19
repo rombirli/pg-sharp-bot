12838b4458cddd08b8ab88913cc7de9add167fce
Romain Birling <romain.birling@epfl.ch>
2022-05-19 20:49:41 +0200

    improved filters

--- processing/filtering.py
+++ processing/filtering.py
@@ -1,27 +1,48 @@
 import numpy as np
 
 
-def all(image: np.array):
-    return (0x00 <= image[:, :, 0]) & (image[:, :, 0] <= 0xff) & \
-           (0x00 <= image[:, :, 1]) & (image[:, :, 1] <= 0xff) & \
-           (0x00 <= image[:, :, 2]) & (image[:, :, 2] <= 0xff)
+
+def filter_color(image: np.array, color: (int, int, int), max_diff: (int, int, int) = (30, 30, 30)):
+    diffr, diffg, diffb = max_diff
+    collr, collg, collb = color
+    return ((collr - diffr) <= image[:, :, 0]) & (image[:, :, 0] <= (collr + diffr)) & \
+           ((collg - diffg) <= image[:, :, 1]) & (image[:, :, 1] <= (collg + diffg)) & \
+           ((collb - diffb) <= image[:, :, 2]) & (image[:, :, 2] <= (collb + diffb))
 
 
-def pokestops(image: np.array):
-    return (20 <= image[:, :, 0]) & (image[:, :, 0] <= 40) & \
-           (100 <= image[:, :, 1]) & (image[:, :, 1] <= 215) & \
-           (220 <= image[:, :, 2]) & (image[:, :, 2] <= 255)
-
-def pgsharp(image: np.array):
-    return (0xf0 <= image[:, :, 0]) & (image[:, :, 0] <= 0xff) & \
-           (0xa0 <= image[:, :, 1]) & (image[:, :, 1] <= 0xcf) & \
-           (0x00 <= image[:, :, 2]) & (image[:, :, 2] <= 0x10)
-
-
-def apply_filter(filter, image):
-    filtered = filter(image)
+def apply_filter(filtered, image):
     image_new = image.copy()
     image_new[:, :, 0] = image_new[:, :, 0] * filtered
     image_new[:, :, 1] = image_new[:, :, 1] * filtered
     image_new[:, :, 2] = image_new[:, :, 2] * filtered
     return image_new
+
+
+class Filters:
+    @classmethod
+    def pgsharp(cls, image: np.array):
+        return filter_color(image, (255, 170, 0)) | \
+               filter_color(image, (255, 208, 11))
+
+
+    @classmethod
+    def pokestops(cls, image: np.array):
+        return filter_color(image, (60, 240, 255)) | \
+               filter_color(image, (30, 117, 230))
+
+    @classmethod
+    def sea(cls, image: np.array):
+        return filter_color(image, (38, 78, 133), (10, 10, 10)) | \
+               filter_color(image, (67, 89, 125), (10, 10, 10))
+
+    @classmethod
+    def road_grey(cls, image: np.array):
+        return filter_color(image, (79, 147, 136))
+
+    @classmethod
+    def road_yellow(cls, image: np.array):
+        return filter_color(image, (255, 255, 148))
+
+    @classmethod
+    def land(cls, image: np.array):
+        return filter_color(image, (11, 165, 135), (70, 70, 40))
--- test.py
+++ test.py
@@ -1,6 +1,26 @@
 import matplotlib.pyplot as plt
 
 from pgsharp.actions import open_pokestop
-from processing import filtering, clustering
-from android.controller import play_record
-play_record('turn_pokestop')
+from processing.filtering import Filters
+from android.controller import screenshot
+
+image = screenshot()
+plt.imshow(Filters.pgsharp(image)),
+plt.title('pgsharp')
+plt.show()
+plt.imshow(Filters.pokestops(image))
+plt.title('pokestops')
+plt.show()
+plt.imshow(Filters.sea(image))
+plt.title('sea')
+plt.show()
+plt.imshow(Filters.road_grey(image))
+plt.title('road grey')
+plt.show()
+plt.imshow(Filters.road_yellow(image))
+plt.title('road yellow')
+plt.show()
+plt.imshow(Filters.land(image))
+plt.title('land')
+plt.show()
+
